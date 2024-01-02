d075c1b9933d7988ea885aac138d7b0769cc8782
Piyush <pjain@truemark.io>
2024-01-02 22:48:05 +0530

    initial commit

---
+++ lib/construct.ts
@@ -0,0 +1,82 @@
+import {Construct} from "constructs";
+import * as path from "path";
+import {
+  aws_iam as iam,
+  aws_lambda as lambda
+} from "aws-cdk-lib";
+
+export interface DatabaseCollectorProps {
+}
+
+export class DatabaseCollector extends Construct {
+  private IAMRole() {
+    const role = new iam.Role(this, "Role", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com")
+    })
+
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecrets"
+        ],
+        resources: ["*"]
+      })
+    )
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "secretsmanager:GetSecretValue",
+        ],
+        resources: ["*"],
+        conditions: {
+          "StringEquals": {"database-collector:enabled": "true"}
+        }
+      })
+    )
+    role.addManagedPolicy({
+      managedPolicyArn: "arn:aws:iam::aws:policy/CloudWatchFullAccessV2"
+    })
+  }
+
+  /**
+   * buildAndInstallGOLambda build the code and create the lambda
+   * @param id - CDK id for this lambda
+   * @param lambdaPath - Location of the code
+   * @param handler - name of the handler to call for this lambda
+   */
+  private buildAndInstallGOLambda(id: string, lambdaPath: string, handler: string) {
+    const environment = {
+      CGO_ENABLED: '0',
+      GOOS: 'linux',
+      GOARCH: "arm64",
+    };
+    return new lambda.Function(this, id, {
+      code: lambda.Code.fromAsset(lambdaPath, {
+        bundling: {
+          image: lambda.Runtime.PROVIDED_AL2023.bundlingImage,
+          user: "root",
+          environment,
+          command: [
+            'bash', '-c', [
+              'curl -LO https://go.dev/dl/go1.21.5.linux-arm64.tar.gz',
+              'rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.5.linux-arm64.tar.gz',
+              'export PATH=$PATH:/usr/local/go/bin',
+              'make vendor',
+              'make lambda-build'
+            ].join(' && ')
+          ]
+        }
+      }),
+      handler,
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+      architecture: lambda.Architecture.ARM_64
+    })
+  }
+
+  constructor(scope: Construct, id: string, props: DatabaseCollectorProps) {
+    super(scope, id);
+    this.IAMRole()
+    this.buildAndInstallGOLambda("database-collector", path.join(__dirname, "../lambda/"), "main")
+  }
+}
--- lib/database-collector-stack.ts
+++ lib/database-collector-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
+import {DatabaseCollector} from "./construct"
 
 export class DatabaseCollectorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // TODO add code here
+    new DatabaseCollector(this, "DatabaseCollector", {
+    })
   }
 }
